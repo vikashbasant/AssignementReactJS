b2ba6481caa55acf9f73ce9fd2baad70337608ea
Vikash Basant <basantvikash360@gmail.com>
2023-06-08 17:16:03 +0530

    Assignement project of ReactJS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>S K Srivastava</h1>
+    <p>Hello! Ambition</p>
+    <ol>
+      <li>Asprint</li>
+      <li>Fresher</li>
+      <li>Mirjapur</li>
+      <li>aaghor</li>
+      <li>Tandav</li>
+    </ol>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
